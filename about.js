f381ae91192781c1849cb9d9606ef79154d13fb5
Eric Shields <me@ericrshields.com>
2025-08-04 15:57:11 -0700

    Initial commit - Tabs Outliner chrome store files

---
+++ about.js
@@ -0,0 +1,10 @@
+/** @preserve Copyright 2012, 2013, 2014, 2015 by Vladyslav Volovyk. All Rights Reserved. */
+
+document.getElementById('extensionVersionString').innerText  = chrome.runtime.getManifest().version;
+
+//FF_REMOVED_GA ga_screenview('About');
+
+//twiter button
+//!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+
+
